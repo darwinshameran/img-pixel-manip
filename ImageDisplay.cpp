64fc1d389e4f3e27ccd8327fe83d59b05a4f8e33
Darwin Shameran <dxsh@riseup.net>
2018-12-11 18:01:23 +0100

    Implement function convert_to_rgba()

--- ImageDisplay.cpp
+++ ImageDisplay.cpp
@@ -1,10 +1,11 @@
 #include "ImageDisplay.h"
 
-ImageDisplay::ImageDisplay(const char* window_title, int width, int height, int channels) noexcept(false) {
+ImageDisplay::ImageDisplay(const char* window_title, int width, int height, int channels) {
     m_window_title = window_title;
     m_width = width;
     m_height = height;
     m_channels = channels;
+    m_pitch = width * 4;
 
     /* Initialize SDL */
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -21,7 +22,55 @@ ImageDisplay::ImageDisplay(const char* window_title, int width, int height, int 
     m_screen = SDL_GetWindowSurface(m_window);
 }
 
-ImageDisplay::~ImageDisplay() {
+/*
+ * A truecolor PNG image uses three seperate values for each
+ * pixel (R, G, B) each being 8 bits. Each of these values belong to a
+ * channel. Thus a png24 image has three channels (24 / 8), and a greyscale
+ * png8 (8 / 8) has one channel. A special channel exists for Alpha, however
+ * this channel does not provide any color but rather decides the opacity
+ * for each pixel.
+ *
+ * To seamlessly use SDL_CreateRGBSurfaceFrom() we pack the bits by shifting
+ * them to ensure a valid 0xAABBGGRR hex. (0xRRGGBBAA is reversed due to
+ * endianness.)
+ *
+ * Ref: Compressed Image File Formats: JPEG, PNG, GIF, XBM, BMP, John Miano
+ *      https://books.google.se/books/about/Compressed_Image_File_Formats.html?id=_nJLvY757dQC
+ *
+ */
+void ImageDisplay::convert_to_rgba(unsigned char** data) {
+    m_pixels = new unsigned[m_height * m_width];
+
+    switch (m_channels) {
+        case 1: {   /* png8 */
+                    for (int y = 0; y < m_height; y++)
+                        for (int x = 0; x < m_width; x++) {
+                            unsigned L = data[y][x];
+                            m_pixels[y * m_width + x] = 0xff000000 | L << 16 | L << 8 | L;
+                        }
+                    break;
+                }
+        case 3: {   /* png24 */
+                    for (int y = 0; y < m_height; y++)
+                        for (int x = 0; x < m_width; x++) {
+                            unsigned R = data[y][x * 3 + 0];
+                            unsigned G = data[y][x * 3 + 1];
+                            unsigned B = data[y][x * 3 + 2];
+                            m_pixels[y * m_width + x] = 0xff000000 | B << 16 | G << 8 | R;
+                        }
+                    break;
+                }
+        case 4: {   /* png32 */
+                    for (int i = 0; i < m_height; i++)
+                        memcpy(m_pixels + i * m_width, data[i], m_pitch);
+                    break;
+                }
+    }
 }
 
+}
+
+ImageDisplay::~ImageDisplay() {
+    delete[] m_pixels;
+}
 /* vim: set ts=4 sw=4 tw=79 ft=cpp et :*/
