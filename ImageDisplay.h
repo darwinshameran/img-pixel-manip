2422e215001cf2a3de730b20b9ef474fb4656b40
Darwin Shameran <dxsh@riseup.net>
2018-12-11 18:36:18 +0100

    Introduce member variable m_pitch; Remove noexcept specifier

--- ImageDisplay.h
+++ ImageDisplay.h
@@ -10,15 +10,16 @@ class ImageDisplay {
     int m_height;
     int m_width;
     int m_channels;
+    int m_pitch;
     unsigned int* m_pixels;
     const char* m_window_title;
 
     SDL_Surface* m_screen;
     SDL_Window* m_window;
 
-    void convert_to_rgba(unsigned char** pixels);
+    void convert_to_rgba(unsigned char**);
  public:
-    ImageDisplay(const char*, int, int, int) noexcept(false);
+    ImageDisplay(const char*, int, int, int);
     ~ImageDisplay();
     void display_image(unsigned char**);
 };
