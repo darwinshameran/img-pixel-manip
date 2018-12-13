02471b38daf29915159b106655e245daafea0bc9
Darwin Shameran <dxsh@riseup.net>
2018-12-13 11:23:09 +0100

    Implement function replace_pixels()

--- Filter.cpp
+++ Filter.cpp
@@ -4,8 +4,17 @@ unsigned char* Filter::get_pixel(const int pos, unsigned char** pixels) const {
     return pixels[pos];
 }
 
-void Filter::set_rgba(int pos, unsigned char new_value, unsigned char* pixels) const {
-    pixels[pos] = new_value;
+void Filter::set_rgba(int pos, unsigned char new_value, unsigned char* pixel) const {
+    pixel[pos] = new_value;
+}
+
+void Filter::replace_pixels(unsigned char** old_pixels, unsigned char** new_pixels, const int size) const {
+    for (int i = 0; i < size; i++) {
+        delete[] old_pixels[i];
+        old_pixels[i] = new_pixels[i];
+    }
+
+    delete[] new_pixels;
 }
 
 int Filter::clamp(const int rgba_value) const {
