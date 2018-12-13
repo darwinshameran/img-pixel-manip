1d7aaab8ab0f760834343d13a52dbd516a717a94
Darwin Shameran <dxsh@riseup.net>
2018-12-13 11:06:39 +0100

    set_pixel() -> set_rgba(); Implement function set_rgba()

--- Filter.h
+++ Filter.h
@@ -5,7 +5,7 @@
 class Filter {
  protected:
      unsigned char* get_pixel(const int pos, unsigned char** pixels) const;
-     void set_pixel(const int x, const int y, unsigned char** pixels) const;
+     void set_rgba(int pos, unsigned char new_value, unsigned char* pixels) const;
      void replace_pixel(unsigned char* old_pixel, unsigned char* pixel, const int y) const;
      
      int clamp(const int rgba_value) const;
