119241b4fd97e90d57d7fafc681d9c637172d92e
Darwin Shameran <dxsh@riseup.net>
2018-12-13 11:24:45 +0100

    Use base class functions for pixel manipulation

--- Blur.cpp
+++ Blur.cpp
@@ -10,9 +10,9 @@ void Blur::apply_filter(int width, int height, int channels, unsigned char** pix
         tmp[i] = new unsigned char[stride];
 
     for (int y = 0; y < height; y++) {
-        unsigned char* row0 = pixels[y == 0 ? y : y - 1];
-        unsigned char* row1 = pixels[y];
-        unsigned char* row2 = pixels[y == height - 1 ? y : y + 1];
+        unsigned char* row0 = get_pixel(y == 0 ? y : y - 1, pixels);
+        unsigned char* row1 = get_pixel(y, pixels);
+        unsigned char* row2 = get_pixel(y == height - 1 ? y : y + 1, pixels);
 
         if (y == 0)
             row0 = row1;
@@ -20,7 +20,7 @@ void Blur::apply_filter(int width, int height, int channels, unsigned char** pix
         if (y == height - 1)
             row2 = row1;
 
-        unsigned char* dest = tmp[y];
+        unsigned char* dest = get_pixel(y, tmp);
 
         for (int i = 0; i < stride; i++) {
             int col0 = i - channels;
@@ -33,20 +33,14 @@ void Blur::apply_filter(int width, int height, int channels, unsigned char** pix
             if (i >= stride - channels)
                 col2 = col1;
 
-            dest[i] = (row0[col0] + row0[col1] + row0[col2]
+            set_rgba(i, (row0[col0] + row0[col1] + row0[col2]
                     + row1[col0] + row1[col1] + row1[col2]
-                    + row2[col0] + row2[col1] + row2[col2]) / 9;
+                    + row2[col0] + row2[col1] + row2[col2]) / 9, dest);
         }
     }
 
     /* Replace pixels in m_pixels with blurred ones. */
-    for (int i = 0; i < height; i++) {
-        delete[] pixels[i];
-        pixels[i] = tmp[i];
-    }
-
-    delete[] tmp;
-
+    replace_pixels(pixels, tmp, height);
 }
 
 Blur::~Blur() {
