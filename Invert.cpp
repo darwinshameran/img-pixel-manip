a104d382128743a0195498edfb8fa4efcb52e4d3
Darwin Shameran <dxsh@riseup.net>
2018-12-13 16:22:54 +0100

    Make use of inherited functions for pixel manipulation

--- Invert.cpp
+++ Invert.cpp
@@ -4,13 +4,12 @@
 
 void Invert::apply_filter(int width, int height, int channels, unsigned char** pixels) {
     for (int i = 0; i < height; i++) {
-        auto row = pixels[i];
         for (int j = 0; j < width; j++) {
-            auto px = &(row[j * channels]);
+            auto px = get_pixel(i, j * channels, pixels);
 
             /* Invert each channel (R, G, B(, A)) */
-            for (int x = 0; x < channels; x++) {
-                px[x] = (255 - px[x]);
+            for (int pos = 0; pos < channels; pos++) {
+                set_rgba(pos, (255 - px[pos]), px);
             }
         }
     }
